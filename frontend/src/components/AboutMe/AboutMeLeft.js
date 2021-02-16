a647830e63341a617b5bbbc198afe44616e45614
koran23 <ahdari33@gmail.com>
2021-02-15 23:21:31 -0700

    edit profile

--- frontend/src/components/AboutMe/AboutMeLeft.js
+++ frontend/src/components/AboutMe/AboutMeLeft.js
@@ -1,42 +1,41 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styled from 'styled-components'
-import {editProfile} from '../../store/profile'
-import Button from '../../styles/Button'
+import styled from "styled-components";
+import { editProfile } from "../../store/profile";
+import Button from "../../styles/Button";
 import { useHistory } from "react-router-dom";
 
 const Left = styled.div`
-
-.about-left{
-    background: ${(props) => props.theme.bg};;
+  .about-left {
+    background: ${(props) => props.theme.bg};
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     height: 100vh;
-}
-.about-left-content > div{
+  }
+  .about-left-content > div {
     background: ${(props) => props.theme.gray};
     padding: 4rem 4rem 2.5rem 5rem;
     text-align: center;
-}
-.about-left-content{
+  }
+  .about-left-content {
     box-shadow: 0px 0px 18px -1px rgba(0, 0, 0, 0.39);
     -webkit-box-shadow: 0px 0px 18px -1px rgba(0, 0, 0, 0.39);
     -moz-box-shadow: 0px 0px 18px -1px rgba(0, 0, 0, 0.39);
-}
-.about-img img{
+  }
+  .about-img img {
     display: block;
     width: 200px;
-}
-.about-img{
+  }
+  .about-img {
     width: 200px;
     height: 200px;
     overflow: hidden;
     border-radius: 50%;
     transition: all 0.5s ease-in-out;
-}
-.shadow{
+  }
+  .shadow {
     margin-left: auto;
     margin-right: auto;
     border-radius: 50%;
@@ -44,8 +43,8 @@ const Left = styled.div`
     height: 200px;
     position: relative;
     cursor: pointer;
-}
-.shadow::after{
+  }
+  .shadow::after {
     content: "";
     position: absolute;
     background: rgba(0, 0, 0, 0.4);
@@ -58,33 +57,33 @@ const Left = styled.div`
     -webkit-filter: blur(3px);
     opacity: 0;
     transition: all 0.6s ease;
-}
-.shadow:hover .about-img{
+  }
+  .shadow:hover .about-img {
     transform: translateY(-10px);
-}
-.shadow:hover::after{
+  }
+  .shadow:hover::after {
     opacity: 1;
-}
-.about-left-content h2{
+  }
+  .about-left-content h2 {
     font-size: 2rem;
     margin: 2.2rem 0 0.6rem 0;
     line-height: 1.2;
     padding-bottom: 1rem;
     border-bottom: 2px solid #edffec;
-}
-.about-left-content h3{
+  }
+  .about-left-content h3 {
     text-transform: uppercase;
     font-weight: 300;
     letter-spacing: 5px;
     margin-top: 1.2rem;
-}
-.icons{
+  }
+  .icons {
     background: #fff;
     display: flex;
     justify-content: center;
     padding: 0.8rem 0;
-}
-.icons li{
+  }
+  .icons li {
     list-style-type: none;
     background: ${(props) => props.theme.primaryColor};
     color: #fff;
@@ -98,11 +97,11 @@ const Left = styled.div`
     font-size: 1.2rem;
     cursor: pointer;
     transition: all 0.5s ease-in-out;
-}
-.icons li:hover{
+  }
+  .icons li:hover {
     background: #edffec;
     color: #000;
-}
+  }
   label input {
     height: 0;
     width: 0;
@@ -124,36 +123,42 @@ const Left = styled.div`
     background: var(--primary);
     color: white;
   }
-    .imgPreview {
+  /* .imgPreview {
     width: auto;
     height: 25px;
     border-radius: 50%;
-  }
+  } */
   button {
-    justify-content: center;
+    padding: 0.4rem 1rem;
+    background: ${(props) => props.theme.darkGrey};
+    color: ${(props) => props.theme.white};
+    border: 1px solid ${(props) => props.theme.blue};
+    border-radius: 3px;
+    text-transform: uppercase;
+    letter-spacing: 1.1px;
   }
-`
+`;
 
 export default function AboutMeLeft() {
-   const history = useHistory();
+  const history = useHistory();
   const dispatch = useDispatch();
   const [imgPreview, setImagePreview] = useState(null);
   const [image, setImage] = useState({ name: null });
-   const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState([]);
 
   const loggedInUser = useSelector((state) => {
     return state.session.user;
   });
 
-    const onClick = (e) => {
-    e.preventDefault()
-    history.push(`/social/${loggedInUser.id}`)
-  }
+  const onClick = (e) => {
+    e.preventDefault();
+    history.push(`/social/${loggedInUser.id}`);
+  };
 
-    const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     let newErrors = [];
-    console.log(image)
+    console.log(image);
     dispatch(editProfile({ userId: loggedInUser.id, image }))
       .then(() => {
         setImage(null);
@@ -166,8 +171,7 @@ export default function AboutMeLeft() {
       });
   };
 
-
-    const updatePost = (e) => {
+  const updatePost = (e) => {
     const file = e.target.files[0];
     if (file) setImage(file);
 
@@ -180,39 +184,63 @@ export default function AboutMeLeft() {
     };
   };
 
-    return (
-        <Left>
-        <div class = "about-left">
-        <div class = "about-left-content">
+  return (
+    <Left>
+      <div class="about-left">
+        <div class="about-left-content">
           <div>
-              <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
               <label>
                 <input type="file" onChange={updatePost} name="image" />
-                <span>+</span>
+                {/* <span>+</span> */}
               </label>
               <img className="imgPreview" src={imgPreview} alt=""></img>
               <br></br>
-              <Button className="contact-form-btn-upload"  type="submit">
-                Upload
+              <Button
+                onClick={onClick}
+                className="contact-form-btn-upload"
+                type="submit"
+              >
+                Edit
               </Button>
+              <br/>
+              <br/>
             </form>
-            <div class = "shadow">
-              <div class = "about-img">
-                <img src = {loggedInUser.profilePic}/>
+            <div class="shadow">
+              <div class="about-img">
+                <img src={loggedInUser.profilePic} />
               </div>
             </div>
-            <h2>{loggedInUser.username}<br></br></h2>
+            <h2>
+              {loggedInUser.username}
+              <br></br>
+            </h2>
             <h3></h3>
           </div>
-          <ul class = "icons">
-            <li><i class = "fab fa-facebook-f"></i></li>
-            <li><i class = "fab fa-twitter"></i></li>
-            <li><i class = "fab fa-linkedin"></i></li>
-            <li><i class = "fab fa-instagram"></i></li>
+          <ul class="icons">
+            <li>
+              <i class="fab fa-facebook-f">
+                <a href={`${loggedInUser.facebook}`} />
+              </i>
+            </li>
+            <li>
+              <i class="fab fa-twitter">
+                <a href={`${loggedInUser.twitter}`} />
+              </i>
+            </li>
+            <li>
+              <i class="fab fa-linkedin">
+                <a href={`${loggedInUser.linkedin}`} />
+              </i>
+            </li>
+            <li>
+              <i class="fab fa-instagram">
+                <a href={`${loggedInUser.instagram}`} />
+              </i>
+            </li>
           </ul>
-          <button onClick={onClick}>Add Socal Media</button>
         </div>
       </div>
-      </Left>
-    )
+    </Left>
+  );
 }
