f1f4c9e0b284e092f9ec0b3b6bfe6584a770d4d5
koran23 <ahdari33@gmail.com>
2021-03-13 18:42:06 -0700

    connection

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,6 +2,7 @@ module.exports = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
   db: {
+    connection: process.env.DATABASE_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
